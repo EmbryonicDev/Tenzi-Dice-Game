87cd788a5eef567388e4fb10baa8b7875a67ca64
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-15 14:45:53 +0200

    Create randomDie to return random number between 1 - 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import Die from "./Die";
+
 function App() {
+
+  function randomDie() {
+    return (
+      Math.ceil(Math.random() * 6)
+    )
+  }
+
   return (
     <main id="main">
       <div id="board"></div>

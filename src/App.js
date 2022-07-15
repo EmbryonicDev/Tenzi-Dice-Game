2c54f6dd6eadfb0331665756d087904cb6e038c6
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-15 15:55:04 +0200

    Modify allNew Dice to push 10 random numbers to arr

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import Die from "./Die";
 
 function App() {
 
-  function allNewDies() {
-    return (
-      Math.ceil(Math.random() * 6)
-    )
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
   }
+  console.log(allNewDice())
 
   return (
     <main id="main">
       <div id="board">
         <div id='dieSet'>
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
-          <Die value={allNewDies()} />
+          <Die value={3} />
+          <Die value={6} />
+          <Die value={1} />
+          <Die value={4} />
+          <Die value={5} />
+          <Die value={5} />
+          <Die value={4} />
+          <Die value={1} />
+          <Die value={3} />
+          <Die value={6} />
         </div>
       </div>
     </main>

689a080e51e596d5b1f688f7c09256a10b42049f
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-18 13:32:49 +0200

    Add title & game instructions + styling

--- src/App.js
+++ src/App.js
@@ -61,6 +61,8 @@ function App() {
   return (
     <main id="main">
       <div id="board">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div id='dieSet'>{diceElements}</div>
         <button id='rollBtn' onClick={rollDice}>Roll</button>
       </div>
